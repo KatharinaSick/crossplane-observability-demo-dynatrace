b9801ba23a98198acca0e90ad9f83861735f28d0
Katharina Sick <katharina.sick@dynatrace.com>
2024-03-13 16:12:01 +0100

    fix: final? fixes

--- charts/kubernetes-workload/terraform-config/log-event.tf
+++ charts/kubernetes-workload/terraform-config/log-event.tf
@@ -9,3 +9,15 @@ resource "dynatrace_log_events" "emergency_logs" {
     davis_merge = true
   }
 }
+
+resource "dynatrace_log_events" "timeout_logs" {
+  enabled = true
+  query   = "dt.kubernetes.cluster.name == \"${var.cluster}\" and k8s.namespace.name == \"${var.namespace}\" and matchesPhrase(content, \"i/o timeout\")"
+  summary = "${var.workload_name} (${var.cluster}/${var.namespace}) - Timeout Log"
+  event_template {
+    title       = "Timeout Logs found in ${var.workload_name} (${var.cluster}/${var.namespace})"
+    description = "We have discovered timeouts in the workload ${var.workload_name} (${var.cluster}/${var.namespace}): {content}"
+    event_type  = "CUSTOM_ALERT"
+    davis_merge = true
+  }
+}
